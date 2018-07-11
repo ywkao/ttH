4ccd467acaaf6a98331190fca5bdd532ce74f0fe
Samuel May <sammay@ucla.edu>
2018-07-11 16:59:36 -0700

    Properly handle MC stats in fit

--- Loopers/tt_template_fit/do_fits_qcd.py
+++ Loopers/tt_template_fit/do_fits_qcd.py
@@ -2,55 +2,105 @@ import os, sys
 import ROOT
 import numpy
 
+def combine_hists(hist1, hist2, name):
+  target_hist = ROOT.TH1D(name, "", hist1.GetNbinsX() + hist2.GetNbinsX(), 0, 1)
+  for i in range(hist1.GetNbinsX()):
+    target_hist.SetBinContent(i+1, hist1.GetBinContent(i+1))
+  for i in range(hist2.GetNbinsX()):
+    target_hist.SetBinContent(hist1.GetNbinsX() + i + 1, hist2.GetBinContent(i+1))
+  for i in range(hist1.GetNbinsX() + hist2.GetNbinsX()):
+    if target_hist.GetBinContent(i+1) < 0:
+      target_hist.SetBinContent(i+1, 0)
+
+  return target_hist
+
+
+latex_dict = { "DiPhoton" : "$ \\gamma \\gamma $ + jets", "GammaJets" : "$ \\gamma $ + jets", "QCD" : "QCD"}
+
 pp_template = "DiPhoton"
 fp_template = "GammaJets"
 ff_template = "QCD"
 
-hist = "hPhotonMinIDMVA"
-#hist = "hPhotonSubleadIDMVA"
+hist_names = ["hPhotonMinIDMVA", "hPhotonMaxIDMVA"]
+templates_dict = { "data" : [["Data"], ""], 
+		"pp" : [["DiPhoton"], "GenPhoton_2"], 
+		"fp" : [["GammaJets"], "GenPhoton_1"], 
+		"ff" : [["QCD"], "GenPhoton_0"], 
+		"bkg" : [["TTGG", "DY", "TTGJets", "VG", "TTJets"], ""] 
+}
 
-#f = ROOT.TFile("../ttbar_cr_v2_histograms.root")
+hists = {}
+
 f = ROOT.TFile("../ttHHadronicLoose_histograms.root")
-h_data = f.Get(hist + "_Data")
-h_pp = f.Get(hist + "_" + pp_template + "GenPhoton_2")
-h_fp = f.Get(hist + "_" + fp_template + "GenPhoton_1")
-h_ff = f.Get(hist + "_" + ff_template + "GenPhoton_0")
 
-h_other = 0
-bkgs = ["DY", "TTGG", "TTGJets", "VG", "TTJets"]
-#bkgs = ["DY", "DiPhoton", "GammaJets", "QCD", "VG", "WJets"]
-for bkg in bkgs:
-  hist_name = hist + "_" + bkg
-  histogram = f.Get(hist_name)
-  if h_other == 0:
-    h_other = histogram
-  else:
-    h_other.Add(histogram)
+hists_unweighted = {} 
+hists_weighted = {}
+hists_weights = {}
 
-mc = ROOT.TObjArray(4)
-mc.Add(h_pp)
-mc.Add(h_fp)
-mc.Add(h_ff)
-mc.Add(h_other)
+did_pp = False
+for template, info in templates_dict.iteritems():
+  #print(template)
+  if template == "pp":
+    did_pp = True
 
-n_bins = h_data.GetSize() - 2
+  hists_unweighted[template] = combine_hists(f.Get(hist_names[0] + "_entries_" + info[0][0] + info[1]), f.Get(hist_names[1] + "_entries_" + info[0][0] + info[1]), template + "unweighted")
+  hists_weighted[template] = combine_hists(f.Get(hist_names[0] + "_" + info[0][0] + info[1]), f.Get(hist_names[1] + "_" + info[0][0] + info[1]), template + "weighted")
+  for i in range(len(info[0]) - 1):
+    hists_unweighted[template].Add(combine_hists(f.Get(hist_names[0] + "_entries_" + info[0][i+1] + info[1]), f.Get(hist_names[1] + "_entries_" + info[0][i+1] + info[1]), template + "unweighted" + str(i)))
+    hists_weighted[template].Add(combine_hists(f.Get(hist_names[0] + "_" + info[0][i+1] + info[1]), f.Get(hist_names[1] + "_" + info[0][i+1] + info[1]), template + "weighted" + str(i)))
 
-hist_list = [h_pp, h_fp, h_ff, h_other]
+  hists_weights[template] = hists_weighted[template].Clone(template + "weights")
+  hists_weights[template].Divide(hists_unweighted[template])
+  for i in range(hists_weights[template].GetNbinsX()):
+    #print(template)
+    #print("Entries: %.2f" % hists_unweighted.GetBinContent(i+1))
+    #print("Weighted content:  %.2f" % hists_weighted.GetBinContent(i+1))
+    #print(hists_weights[template].GetBinContent(i+1))
+    if hists_weights[template].GetBinContent(i+1) <= 0:
+       hists_weights[template].SetBinContent(i+1, 0.000000001)
+  hists[template] = [ hists_unweighted[template], hists_weighted[template], hists_weights[template] ]
+  #if did_pp:
+    #print hists["pp"][2].GetBinContent(10)
+    #for i in range(hists_weights.GetNbinsX()):
+    #  print hists["pp"][2].GetBinContent(i+1)
+
+
+#print(hists["pp"][2].GetBinContent(9))
+
+hist_unweighted_list = [hists["ff"][0], hists["fp"][0], hists["pp"][0], hists["bkg"][0]]
+hist_weighted_list = [hists["ff"][1], hists["fp"][1], hists["pp"][1], hists["bkg"][1]]
+hist_weights_list = [hists["ff"][2], hists["fp"][2], hists["pp"][2], hists["bkg"][2]]
+
+#print(hist_weights_list[2].GetBinContent(9))
+
+h_data =  hists["data"][0]
 initial_fracs = []
+for i in range(len(hist_weighted_list)):
+  print(i)
+  print(hist_unweighted_list[i].Integral())
+  print(hist_weighted_list[i].Integral())
+  print(hist_weights_list[i].GetBinContent(9))
+  initial_fracs.append(hist_weighted_list[i].Integral() / h_data.Integral())
 
-for entry in hist_list:
-  initial_fracs.append(entry.Integral()/h_data.Integral())
-  for i in range(n_bins):
-    if entry.GetBinContent(i+1) < 0:
-      entry.SetBinContent(i+1, 0)
+print(initial_fracs)
+
+# Fit
+mc = ROOT.TObjArray(4)
+mc.Add(hists["ff"][0])
+mc.Add(hists["fp"][0])
+mc.Add(hists["ff"][0])
+mc.Add(hists["bkg"][0])
+
+hist_unweighted_list = [hists["ff"][0], hists["fp"][0], hists["pp"][0], hists["bkg"][0]]
+hist_weights_list = [hists["ff"][2], hists["fp"][2], hists["pp"][2], hists["bkg"][2]]
 
 fit = ROOT.TFractionFitter(h_data, mc, "Q")
 for i in range(4):
-  if i == 3:
+  fit.SetWeight(i, hist_weights_list[i]) 
+  if i == 3: # other bkgs, fix
     fit.Constrain(i, initial_fracs[i]-0.00000001, initial_fracs[i]+0.00000001)
   else:
     fit.Constrain(i, 0.0, 1.0)
-  #fit.Constrain(i, initial_fracs[i]-0.25, initial_fracs[i]+0.25)
 
 fit.Fit()
 
@@ -65,13 +115,16 @@ for i in range(4):
   fracs.append(f)
   frac_errs.append(ferr)
   scales.append(f/initial_fracs[i])
-  #scales.append(initial_fracs[i]/f)
-
 
 print "Initial fractions", initial_fracs
 print "Fitted fractions", fracs
 print "Errors", frac_errs
-
 print "Scales" , scales
 
-del fit
+print "\\begin{center} \\Fontvi \\begin{tabular}{|l|| r| r| r|} \\hline"
+print "Template & Initial Fraction & Fitted Fraction & Scale \\\\ \\hline"
+print "%s & %.2f & %.2f $ \\pm $ %.2f & %.2f \\\\" % (latex_dict[ff_template] + " (fake/fake)", initial_fracs[0], fracs[0], frac_errs[0], scales[0])
+print "%s & %.2f & %.2f $ \\pm $ %.2f & %.2f \\\\" % (latex_dict[fp_template] + " (fake/prompt)", initial_fracs[1], fracs[1], frac_errs[1], scales[1])
+print "%s & %.2f & %.2f $ \\pm $ %.2f & %.2f \\\\ \\hline" % (latex_dict[pp_template] + " (prompt/prompt)", initial_fracs[2], fracs[2], frac_errs[2], scales[2])
+print "\\end{tabular} \\end{center}"
+
