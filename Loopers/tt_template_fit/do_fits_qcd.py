d7d1c88f0e5517cda7489f0fe91653c5173d7a84
Samuel May <sammay@ucla.edu>
2018-07-12 11:59:00 -0700

    Clean up fit script, fix a :bug

--- Loopers/tt_template_fit/do_fits_qcd.py
+++ Loopers/tt_template_fit/do_fits_qcd.py
@@ -37,12 +37,8 @@ hists_unweighted = {}
 hists_weighted = {}
 hists_weights = {}
 
-did_pp = False
+# Grab our histos
 for template, info in templates_dict.iteritems():
-  #print(template)
-  if template == "pp":
-    did_pp = True
-
   hists_unweighted[template] = combine_hists(f.Get(hist_names[0] + "_entries_" + info[0][0] + info[1]), f.Get(hist_names[1] + "_entries_" + info[0][0] + info[1]), template + "unweighted")
   hists_weighted[template] = combine_hists(f.Get(hist_names[0] + "_" + info[0][0] + info[1]), f.Get(hist_names[1] + "_" + info[0][0] + info[1]), template + "weighted")
   for i in range(len(info[0]) - 1):
@@ -52,51 +48,30 @@ for template, info in templates_dict.iteritems():
   hists_weights[template] = hists_weighted[template].Clone(template + "weights")
   hists_weights[template].Divide(hists_unweighted[template])
   for i in range(hists_weights[template].GetNbinsX()):
-    #print(template)
-    #print("Entries: %.2f" % hists_unweighted.GetBinContent(i+1))
-    #print("Weighted content:  %.2f" % hists_weighted.GetBinContent(i+1))
-    #print(hists_weights[template].GetBinContent(i+1))
     if hists_weights[template].GetBinContent(i+1) <= 0:
        hists_weights[template].SetBinContent(i+1, 0.000000001)
   hists[template] = [ hists_unweighted[template], hists_weighted[template], hists_weights[template] ]
-  #if did_pp:
-    #print hists["pp"][2].GetBinContent(10)
-    #for i in range(hists_weights.GetNbinsX()):
-    #  print hists["pp"][2].GetBinContent(i+1)
 
-
-#print(hists["pp"][2].GetBinContent(9))
 
 hist_unweighted_list = [hists["ff"][0], hists["fp"][0], hists["pp"][0], hists["bkg"][0]]
 hist_weighted_list = [hists["ff"][1], hists["fp"][1], hists["pp"][1], hists["bkg"][1]]
 hist_weights_list = [hists["ff"][2], hists["fp"][2], hists["pp"][2], hists["bkg"][2]]
 
-#print(hist_weights_list[2].GetBinContent(9))
-
-h_data =  hists["data"][0]
+h_data =  hists["data"][1]
 initial_fracs = []
-for i in range(len(hist_weighted_list)):
-  print(i)
-  print(hist_unweighted_list[i].Integral())
-  print(hist_weighted_list[i].Integral())
-  print(hist_weights_list[i].GetBinContent(9))
-  initial_fracs.append(hist_weighted_list[i].Integral() / h_data.Integral())
-
-print(initial_fracs)
+for hist in hist_weighted_list:
+  initial_fracs.append(hist.Integral() / h_data.Integral())
 
 # Fit
 mc = ROOT.TObjArray(4)
 mc.Add(hists["ff"][0])
 mc.Add(hists["fp"][0])
-mc.Add(hists["ff"][0])
+mc.Add(hists["pp"][0])
 mc.Add(hists["bkg"][0])
-
-hist_unweighted_list = [hists["ff"][0], hists["fp"][0], hists["pp"][0], hists["bkg"][0]]
-hist_weights_list = [hists["ff"][2], hists["fp"][2], hists["pp"][2], hists["bkg"][2]]
 
 fit = ROOT.TFractionFitter(h_data, mc, "Q")
 for i in range(4):
-  fit.SetWeight(i, hist_weights_list[i]) 
+  fit.SetWeight(i, hist_weights_list[i]) # set bin-by-bin weights for raw MC counts 
   if i == 3: # other bkgs, fix
     fit.Constrain(i, initial_fracs[i]-0.00000001, initial_fracs[i]+0.00000001)
   else:
@@ -111,16 +86,17 @@ for i in range(4):
   f = ROOT.Double()
   ferr = ROOT.Double()
   fit.GetResult(i, f, ferr)
-  print i, f, ferr
   fracs.append(f)
   frac_errs.append(ferr)
   scales.append(f/initial_fracs[i])
 
+# Print fit results
 print "Initial fractions", initial_fracs
 print "Fitted fractions", fracs
 print "Errors", frac_errs
 print "Scales" , scales
 
+# Make pretty table
 print "\\begin{center} \\Fontvi \\begin{tabular}{|l|| r| r| r|} \\hline"
 print "Template & Initial Fraction & Fitted Fraction & Scale \\\\ \\hline"
 print "%s & %.2f & %.2f $ \\pm $ %.2f & %.2f \\\\" % (latex_dict[ff_template] + " (fake/fake)", initial_fracs[0], fracs[0], frac_errs[0], scales[0])
