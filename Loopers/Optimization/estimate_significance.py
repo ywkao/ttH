9edd8a0a8f3709292a916278c4158881236d02f0
Samuel May <sammay@ucla.edu>
2018-07-25 13:46:44 -0700

    Update significance estimate to use effective sigma for s and b estiamtes

--- Loopers/Optimization/estimate_significance.py
+++ Loopers/Optimization/estimate_significance.py
@@ -19,10 +19,6 @@ def Z_A(s, b):
     b = 0
   return math.sqrt(2 * ( ((s+b) * math.log(1 + (s/b) )) - s) ) 
 
-#print(Z_A(1.95*0.9, 1.1))
-#print(Z_A(4.74*0.9, 11.69))
-#print(Z_A(4.09*0.9, 10.11))
-
 def find_nearest(array,value):
     val = numpy.ones_like(array)*value
     idx = (numpy.abs(array-val)).argmin()
@@ -50,32 +46,44 @@ quantiles, mva_cut = quantiles_to_mva_score(n_quantiles, signal_mva_scores)
 quants = []
 sig_mc = []
 sig_data = []
+
 # for each signal efficiency between 0-100%, calculate Z_A
+selection_signal = "label_ == 1"
+selection_bkg = "label_ == 0"
+selection_data = "label_ == 2"
+
 for i in range(len(quantiles)):
-  selection_signal = "label_ == 1 && mass_ >= 122 && mass_ <= 128 && mva_score_ >= %.6f" % mva_cut[i][0]
-  sig_events = root_numpy.tree2array(tree, branches = "evt_weight_", selection = selection_signal )
-  selection_bkg = "label_ == 0 && mass_ >= 122 && mass_ <= 128 && mva_score_ >= %.6f" % mva_cut[i][0]
-  bkg_events = root_numpy.tree2array(tree, branches = "evt_weight_", selection = selection_bkg )
-  selection_data = "label_ == 2 && mass_ >= 100 && mass_ <= 180 && mva_score_ >= %.6f" % mva_cut[i][0]
-  data_events = root_numpy.tree2array(tree, branches = "mass_", selection = selection_data)
+  selection_base = "mva_score_ >= %.6f" % mva_cut[i][0]
 
+  sig_mass = root_numpy.tree2array(tree, branches = "mass_", selection = selection_signal + " && " + selection_base) 
+  sig_weights = root_numpy.tree2array(tree, branches = "evt_weight_", selection = selection_signal + " && " + selection_base) 
 
-  if (len(sig_events) == 0):
-    continue
+  # calculate effective width
+  mean_eff, sigma_eff = utils.calc_sigma_eff(sig_mass, sig_weights, i)
+  selection_mass = "mass_ >= %.6f && mass_ <= %.6f" % (mean_eff - (1.675 * sigma_eff), mean_eff + (1.675 * sigma_eff))
 
+  # calculate s
+  sig_events = root_numpy.tree2array(tree, branches = "evt_weight_", selection = selection_signal + " && " + selection_base + " && " + selection_mass)
   s = numpy.sum(sig_events)
-  if s < 1:
-    continue
-  b_mc = numpy.sum(bkg_events)
-  b_data = utils.fit_exp(data_events, i)
-  z_mc = Z_A(s*0.9, b_mc) # multiply signal yield by 90%, per email from Andrea
-  z_data = Z_A(s*0.9, b_data)
-  print s, b_mc, b_data, z_mc, z_data
-  if z_mc > 0 and z_mc < 100 and z_data > 0 and z_data < 100:
-    quants.append(quantiles[i])
-    sig_mc.append(z_mc)
-    sig_data.append(z_data)
 
+  # calculate b from mc
+  bkg_events = root_numpy.tree2array(tree, branches = "evt_weight_", selection = selection_bkg + " && " + selection_base + " && " + selection_mass)
+  b_mc = numpy.sum(bkg_events)
+
+  # calculate b from fit to data sidebands
+  data_events = root_numpy.tree2array(tree, branches = "mass_", selection = selection_data + " && " + selection_base)
+  if len(data_events) < 5:
+    continue # fit doesn't seem to work with less than 5 events
+  b_data = utils.fit_exp(data_events, mean_eff, sigma_eff, i)
+
+  z_mc = Z_A(s*0.9, b_mc)
+  z_data = Z_A(s*0.9, b_data)
+
+  print s, b_mc, b_data, z_mc, z_data
+
+  quants.append(quantiles[i])
+  sig_mc.append(z_mc)
+  sig_data.append(z_data)
 
 ### Make diagnostic plots ###
 import matplotlib
@@ -87,6 +95,6 @@ plt.plot(quants, sig_mc, label='Estimate bkg from MC')
 plt.plot(quants, sig_data, label='Estimate bkg from data')
 plt.xlabel('Signal Efficiency')
 plt.ylabel('Significance (Z_A)')
-plt.ylim([0.0, 2.0])
+plt.ylim([0.0, 3.0])
 plt.legend(loc='upper right')
 plt.savefig('optimization.pdf')
--- Loopers/Optimization/utils.py
+++ Loopers/Optimization/utils.py
@@ -2,6 +2,8 @@ import ROOT
 import numpy
 import math
 from scipy.optimize import curve_fit
+from scipy.integrate import quad
+
 
 import matplotlib
 matplotlib.use('Agg')
@@ -10,7 +12,41 @@ import matplotlib.pyplot as plt
 def exp(x, a, b):
   return a * numpy.exp(-b*x) 
 
-def fit_exp(data, idx):
+def gaus(x, a, b, c):
+  return a * numpy.exp(-0.5 * (( (x - b) / c ) ** 2))
+
+def calc_sigma_eff(signal_data, weights, idx):
+  h = ROOT.TH1D("h_sig", "", 80, 100, 180)
+  for i in range(len(signal_data)):
+    h.Fill(signal_data[i], weights[i])
+
+  x_fit = numpy.linspace(120.5, 129.5, 10)
+  y_fit = []
+  y_err = []
+  for i in range(h.GetNbinsX()):
+    if not (h.GetBinCenter(i+1) > 120 and h.GetBinCenter(i+1) < 130):
+      continue
+    y_fit.append(h.GetBinContent(i+1))
+    y_err.append(h.GetBinError(i+1))
+
+  # fit
+  popt, pcov = curve_fit(gaus, x_fit, y_fit, p0 = [1, 125, 2])
+
+  # plot
+  x_plot = numpy.linspace(120, 130, 100)
+  fig = plt.figure()
+  plt.plot(x_plot, gaus(x_plot, *popt), 'r-', label = 'fit')
+  plt.errorbar(x_fit, y_fit, yerr = y_err, label = 'ttH', fmt = 'o')
+  plt.legend(loc = 'upper right')
+  plt.savefig("bkg_fits/sig_fit_%d.pdf" % idx)
+  plt.xlim([120, 130])
+  plt.close()
+
+  del h
+  return popt[1], popt[2] 
+   
+
+def fit_exp(data, mean_eff, sigma_eff, idx):
   h = ROOT.TH1D("h_data", "", 80, 100, 180)
   for data_point in data:
     h.Fill(data_point)
@@ -18,22 +54,19 @@ def fit_exp(data, idx):
   x_fit = numpy.append(numpy.linspace(100, 120, 20), numpy.linspace(130, 180, 50))
   y_fit = []
   for i in range(h.GetNbinsX()):
-    if h.GetBinCenter(i) > 120 and h.GetBinCenter(i) < 130:
+    if h.GetBinCenter(i+1) > 120 and h.GetBinCenter(i+1) < 130:
       continue
     y_fit.append(h.GetBinContent(i+1))
   popt, pcov = curve_fit(exp, x_fit, y_fit, p0 = [10, 0.01])
 
-  mass_low = 122
-  mass_high = 128
-  bkg_pred = 0
-  for mass in range(mass_low, mass_high):
-    bkg_pred += exp(mass, *popt)
+  bkg_pred = quad(exp, mean_eff - (1.675*sigma_eff), mean_eff + (1.675*sigma_eff), args = (popt[0], popt[1]))[0]
 
   fig = plt.figure()
   x = numpy.linspace(100, 180, 80)
   plt.plot(x, exp(x, *popt), 'r-', label='fit')
   plt.errorbar(x_fit, y_fit, yerr = numpy.sqrt(y_fit), label = 'data', fmt='o')  
   plt.legend(loc='upper right')
+  plt.xlim([100, 180])
   plt.savefig("bkg_fits/bkg_fit_%d.pdf" % idx)
   plt.close()
 
