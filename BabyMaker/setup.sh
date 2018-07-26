102fdddac56234b0f3710cf584a5f407b3748c33
Samuel May <sammay@ucla.edu>
2018-07-26 15:20:34 -0700

    Update setup to accomodate 94X processing

--- BabyMaker/setup.sh
+++ BabyMaker/setup.sh
@@ -19,7 +19,7 @@ if [ ! -d flashgg ]; then
   cd $CMSSW_BASE/src
   git clone https://github.com/sam-may/flashgg 
   cd flashgg
-  git checkout tth_dev
+  git checkout tth_dev_80X
 
   cd ..
   source flashgg/setup_8_0_X.sh # ignore the "error: addinfo_cache" messages
@@ -31,6 +31,36 @@ if [ ! -d flashgg ]; then
 else
   cd ../../   
 fi
+
+if [ ! -d CMSSW_9_4_6 ]; then
+  cmsrel CMSSW_9_4_6
+  cd CMSSW_9_4_6/src
+  cmsenv
+  git cms-init
+else
+  cd CMSSW_9_4_6/src
+  cmsenv
+fi
+
+if [ ! -d flashgg ]; then
+  cd $CMSSW_BASE/src
+  git clone https://github.com/sam-may/flashgg
+  cd flashgg
+  git checkout tth_dev_94X
+
+  cd ..
+  source flashgg/setup_9_4_X.sh 
+
+  # Build
+  cd $CMSSW_BASE/src
+  scram b -j 30
+  source flashgg/afterbuild_9_4_X.sh
+  cd ../../
+else
+  cd ../../
+fi
+
+
 
 if [ ! -d logs ]; then
   mkdir logs
