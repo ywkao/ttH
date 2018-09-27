Fix bugs :bug: (proper bkg estimate, proper negative weights for signal), add za calculation to mva training

MVAs/significance_utils.py
@@ -1,3 +1,15 @@
+import numpy
+import ROOT, root_numpy
+import math
+from scipy.optimize import curve_fit
+
+def Z_A(s, b):
+  if s < 0:
+    s = 0
+  if b < 0:
+    b = 0
+  return math.sqrt(2 * ( ((s+b) * math.log(1 + (s/b) )) - s) )
+
 def quantiles_to_mva_score(n_quantiles, mva_scores): # return mva_scores corresponding to each quantile in n_quantiles
   sorted_mva = numpy.flip(numpy.sort(mva_scores), 0)
   quantiles = []
@@ -20,22 +32,7 @@ def gaus(x, a, b, c):
 def constant_estimate(weights, sigma_eff):
   return numpy.sum(weights) * ((2 * 1.645 * sigma_eff) / (180. - 100.))
 
-def constant_estimate(data, weights, mean_eff, sigma_eff, smear):
-  if smear == 0:
-    return numpy.sum(weights) * ((2 * 1.645 * sigma_eff) / (180. - 100.))
-  else:
-    h = ROOT.TH1D("h_sig", "", 1, 100, 180)
-    for i in range(len(data)):
-      h.Fill(data[i], weights[i])
-    mc_yield = h.GetBinContent(1)
-    mc_unc = h.GetBinError(1)
-    if smear == -1:
-      mc_yield += -mc_unc
-    elif smear == 1:
-      mc_yield += mc_unc
-    return mc_yield * ((2 * 1.645 * sigma_eff) / (180. - 100.))
-
-def calc_sigma_eff(signal_data, weights) 
+def calc_sigma_eff(signal_data, weights): 
   h = ROOT.TH1D("h_sig", "", 80, 100, 180)
   for i in range(len(signal_data)):
     h.Fill(signal_data[i], weights[i])
@@ -53,51 +50,51 @@ def calc_sigma_eff(signal_data, weights)
   popt, pcov = curve_fit(gaus, x_fit, y_fit, sigma = y_err, p0 = [1, 125, 2])
 
   del h
-  return popt[1], popt[2]
+  return popt[1], abs(popt[2])
 
 def events_passing_cut(events, cut):
-  events_pass = {"mass" : [], "weight" : [], "mva_score" : []}
+  events_pass = {"mass" : [], "weights" : [], "mva_score" : []}
   for i in range(len(events["mass"])):
     if events["mva_score"][i] > cut:
       events_pass["mass"].append(events["mass"][i])
-      events_pass["weight"].append(events["weight"][i])
+      events_pass["weights"].append(events["weights"][i])
       events_pass["mva_score"].append(events["mva_score"][i])
   return events_pass
-
 
 def za_scores(n_quantiles, signal_events, background_events):
   testing_frac = 0.5 # this assumes that we always use half the mc for testing and half for training
   za = []
   # Calculate cuts corresponding to each quantile
   quantiles, mva_cut = quantiles_to_mva_score(n_quantiles, signal_events["mva_score"])
-  
+
   # For each cut:
   for cut_value in mva_cut:
     # Get events passing cut
-    signal_events_pass = events_pass(signal_events, mva_cut)
-    bkg_events_pass = events_pass(background_events, mva_cut)
+    signal_events_passing_cut = events_passing_cut(signal_events, cut_value)
+    bkg_events_passing_cut = events_passing_cut(background_events, cut_value)
 
     # Calculate sigma eff
-    mean_eff, sigma_eff = calc_sigma_eff(signal_events_pass["mass"], signal_events_pass["weights"])
+    mean_eff, sigma_eff = calc_sigma_eff(signal_events_passing_cut["mass"], signal_events_passing_cut["weights"])
     
     # Calculate s
     signal_events_mass_window = []
-    for i in range(len(signal_events_pass["mass"])):
-      if signal_events_pass["mass"][i] > mean_eff - (1.645 * sigma_eff) and signal_events_pass["mass"][i] < mean_eff + (1.645 * sigma_eff):
-        signal_events_mass_window.append(signal_events_pass["weight"][i])
+    for i in range(len(signal_events_passing_cut["mass"])):
+      if signal_events_passing_cut["mass"][i] > mean_eff - (1.645 * sigma_eff) and signal_events_passing_cut["mass"][i] < mean_eff + (1.645 * sigma_eff):
+        signal_events_mass_window.append(signal_events_passing_cut["weights"][i])
     signal_events_mass_window = numpy.asarray(signal_events_mass_window) 
     s = ( 1. / testing_frac) * numpy.sum(signal_events_mass_window)
 
     # Calculate b
     bkg_events_mass_window = []
-    for i in range(len(bkg_events_pass["mass"])):
-      if bkg_events_pass["mass"][i] > 100 and bkg_events_pass["mass"] < 180:
-        bkg_events_mass_window.append(bkg_events_pass["weight"][i])
+    for i in range(len(bkg_events_passing_cut["mass"])):
+      if bkg_events_passing_cut["mass"][i] > 100 and bkg_events_passing_cut["mass"][i] < 180:
+        bkg_events_mass_window.append(bkg_events_passing_cut["weights"][i])
     bkg_events_mass_window = numpy.asarray(bkg_events_mass_window)
     b = ( 1. / testing_frac) * constant_estimate(bkg_events_mass_window, sigma_eff)
 
     # Calculate Z_A
     z_mc = Z_A(s, b)
+    print s, b, z_mc
     za.append(z_mc)
 
   return za