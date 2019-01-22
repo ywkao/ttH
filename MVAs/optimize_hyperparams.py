0ed6a1532eafa43c568c9065abe3d86953a92e5e
Samuel May <sammay@ucla.edu>
2019-01-22 13:13:38 -0800

    Add scripts for bayesian optimization of hyperparams

---
+++ MVAs/optimize_hyperparams.py
@@ -0,0 +1,118 @@
+import sys
+import json
+import math
+import copy
+
+from bayes_opt import BayesianOptimization
+
+sys.path.append("../Loopers")
+import parallel_utils
+import train_v2
+
+def estimate_max_za(n_round, max_depth, eta, subsample, colsample_bytree, min_child_weight, gamma, reg_alpha, reg_lambda):
+  global idx, config, log
+ 
+  print "%d-th iteration of hyperparameter scan" % idx
+
+  n_round = int(n_round)
+  max_depth = int(max_depth)
+
+  config["n_round"] = n_round
+  config["max_depth"] = max_depth
+  config["eta"] = eta
+  config["subsample"] = subsample
+  config["colsample_bytree"] = colsample_bytree
+  config["min_child_weight"] = min_child_weight
+  config["gamma"] = gamma
+  config["reg_alpha"] = reg_alpha
+  config["reg_lambda"] = reg_lambda
+
+  config_1, config_2 = copy.deepcopy(config), copy.deepcopy(config)
+  config_1["invert_test_and_train"] = True
+  config_2["invert_test_and_train"] = False
+
+  max_za_mc_1, max_za_mc_unc_1, max_za_data_1, max_za_data_unc_1 = train_v2.train_bdt(config_1)
+  max_za_mc_2, max_za_mc_unc_2, max_za_data_2, max_za_data_unc_2 = train_v2.train_bdt(config_2)
+
+  max_za_mc_comb = (max_za_mc_1 + max_za_mc_2) / 2
+  max_za_mc_unc_comb = math.sqrt((max_za_mc_unc_1**2) + (max_za_mc_unc_2**2)) / 2
+
+  if idx == 0:
+    scan_results = {}
+
+  else:
+    with open(log, "r") as f_in:
+      scan_results = json.load(json_file)
+
+  scan_results[idx] = {
+	"config" : config,
+	"max_za_mc_1" : max_za_mc_1,
+	"max_za_mc_unc_1" : max_za_mc_unc_1,
+	"max_za_data_1" : max_za_data_1,
+        "max_za_data_unc_1" : max_za_data_unc_1,
+	"max_za_mc_2" : max_za_mc_2,
+        "max_za_mc_unc_2" : max_za_mc_unc_2,
+        "max_za_data_2" : max_za_data_2,
+        "max_za_data_unc_2" : max_za_data_unc_2,
+	"max_za_mc_comb" : max_za_mc_comb,
+	"max_za_mc_unc_comb" : max_za_mc_unc_comb,
+    }
+
+  with open(log, "w") as f_out:
+    json.dump(scan_results, f_out, indent=4, sort_keys=True)
+
+  idx += 1
+  return max_za_mc_comb
+
+config = {
+        "input_file" : "ttHHadronic_1617_ttHHadronicLoose_features.hdf5",
+        "tag" : "testin",
+
+        "sideband" : False,
+        "optimization_vars" : [],
+        "multiclassifier" : False,
+}
+
+idx = 0
+log = "bayes_hyperparam_scan.json"
+
+pbounds = {
+	"n_round" 		: (1, 1000),
+	"max_depth" 		: (2, 15),
+	"eta" 			: (0.0, 1.0),
+	"subsample" 		: (0.5, 1.0),
+	"colsample_bytree" 	: (0.1, 1.0),
+	"min_child_weight" 	: (0, 20),
+	"gamma" 		: (0, 10),
+	"reg_alpha"		: (0, 10),
+	"reg_lambda"		: (0, 10),
+}
+
+starting_point = {
+	"n_round" 		: 75,
+	"max_depth" 		: 4,
+	"eta" 			: 0.2,
+	"subsample" 		: 1.0,
+	"colsample_bytree" 	: 1.0,
+	"min_child_weight" 	: 1,
+	"gamma" 		: 0,
+	"reg_lambda" 		: 1,
+	"reg_alpha"		: 0,
+}
+
+optimizer = BayesianOptimization(
+    f=estimate_max_za,
+    pbounds=pbounds,
+    verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    random_state=1,
+)
+
+optimizer.probe(params = starting_point, lazy = True)
+
+optimizer.maximize(
+	init_points = 3,
+	n_iter = 10,
+)
+
+for i, res in enumerate(optimizer.res):
+    print("Iteration {}: \n\t{}".format(i, res))
---
+++ MVAs/train_v2.py
@@ -0,0 +1,187 @@
+import os, sys
+import numpy
+import h5py
+import xgboost
+import pandas
+import json
+import math
+
+from sklearn.cross_validation import train_test_split
+from sklearn.utils import shuffle
+from sklearn import metrics
+
+import utils
+import tmva_utils
+import ks_test
+import significance_utils
+
+def train_bdt(config):
+  # Trains BDT with given hyperparams and returns max Z_A (as calculated on bkg MC), requiring at least 4 signal events
+  f = h5py.File(config["input_file"], "r")
+
+  feature_names = utils.load_array(f, 'feature_names')
+  training_feature_names = utils.load_array(f, 'training_feature_names')
+
+  print "Training with the following features: ", training_feature_names
+
+  if config["invert_test_and_train"]:
+    print "Inverting test and train splits"
+    if config["sideband"]:
+      print "Not yet implemented how to handle inverting the test/train set when training on data sidebands, exiting"
+      return -1
+
+    global_features = utils.load_array(f, 'global_validation')
+    label = utils.load_array(f, 'label_validation')
+    multi_label = utils.load_array(f, 'multi_label_validation')
+    weights = utils.load_array(f, 'weights_validation')
+    mass = utils.load_array(f, 'mass_validation')
+ 
+    global_features_validation = utils.load_array(f, 'global')
+    label_validation = utils.load_array(f, 'label')
+    multi_label_validation = utils.load_array(f, 'multi_label')
+    weights_validation = utils.load_array(f, 'weights')
+    mass_validation = utils.load_array(f, 'mass')
+
+  else:
+    global_features = utils.load_array(f, 'global')
+    label = utils.load_array(f, 'label')
+    multi_label = utils.load_array(f, 'multi_label')
+    weights = utils.load_array(f, 'weights')
+    mass = utils.load_array(f, 'mass')
+
+    global_features_validation = utils.load_array(f, 'global_validation')
+    label_validation = utils.load_array(f, 'label_validation')
+    multi_label_validation = utils.load_array(f, 'multi_label_validation')
+    weights_validation = utils.load_array(f, 'weights_validation')
+    mass_validation = utils.load_array(f, 'mass_validation')
+
+  if config["sideband"]:
+    global_features = utils.load_array(f, 'global_data_sideband')
+    label = utils.load_array(f, 'label_data_sideband')
+    multi_label = utils.load_array(f, 'multi_label_data_sideband')
+    weights = utils.load_array(f, 'weights_data_sideband')
+    mass = utils.load_array(f, 'mass_data_sideband')
+
+  global_features_data = utils.load_array(f, 'global_data')
+  label_data = utils.load_array(f, 'label_data')
+  multi_label_data = utils.load_array(f, 'multi_label_data')
+  weights_data = utils.load_array(f, 'weights_data')
+  mass_data = utils.load_array(f, 'mass_data')
+
+  print global_features.shape
+  print label.shape
+  print weights.shape
+
+  print global_features_validation.shape
+  print label_validation.shape
+  print weights_validation.shape
+
+  print global_features_data.shape
+  print label_data.shape
+  print weights_data.shape
+
+  x_train, y_train, y_train_multi, weights_train = global_features, label, multi_label, weights
+  x_test, y_test, y_test_multi, weights_test  = global_features_validation, label_validation, multi_label_validation, weights_validation
+
+  X_train = pandas.DataFrame(data=x_train, columns = training_feature_names)
+  X_test = pandas.DataFrame(data=x_test, columns = training_feature_names)
+  X_data = pandas.DataFrame(data=global_features_data, columns = training_feature_names)
+
+  if config["multiclassifier"]:
+    Y_train = y_train_multi
+    Y_test = y_test_multi
+  else:
+    Y_train = y_train
+    Y_test = y_test
+
+  sum_neg_weights = utils.sum_of_weights_v2(weights_train, label, 0)
+  sum_pos_weights = utils.sum_of_weights_v2(weights_train, label, 1)
+
+  print sum_pos_weights, sum_neg_weights
+
+  d_train = xgboost.DMatrix(X_train, label = Y_train, weight = weights_train)
+  d_test = xgboost.DMatrix(X_test, label = Y_test)
+  d_data = xgboost.DMatrix(X_data)
+
+  param = {
+	'max_depth': config["max_depth"],
+        'eta': config["eta"],
+        'subsample': config["subsample"],
+        'colsample_bytree': config["colsample_bytree"],
+        'min_child_weight' : config["min_child_weight"],
+	'gamma' : config["gamma"],
+	'reg_alpha' : config["reg_alpha"],
+	'reg_lambda' : config["reg_lambda"],
+
+	'scale_pos_weight': sum_neg_weights / sum_pos_weights,    
+	'objective': 'binary:logistic',
+	'nthread' : 16,
+  } 
+
+  if config["multiclassifier"]:
+    param["num_class"] = config["n_class"]
+    param["objective"] = "multi:softprob"
+    param["scale_pos_weight"] = 1
+
+  evallist = [(d_train, 'train'), (d_test, 'test')]
+  progress = {}
+
+  n_round = config["n_round"]
+  print param, n_round
+
+  # train
+  bdt = xgboost.train(param, d_train, n_round, evallist, evals_result = progress)
+
+  bdt.save_model(config["tag"] + "_bdt.xgb")
+  model = bdt.get_dump()
+
+  input_variables = []
+  for name in feature_names:
+    input_variables.append((name, 'F'))
+  tmva_utils.convert_model(model, input_variables = input_variables, output_xml = config["tag"] + '_bdt.xml')
+  
+  # predict
+  pred_train = bdt.predict(d_train, output_margin=config["multiclassifier"])
+  pred_test = bdt.predict(d_test, output_margin=config["multiclassifier"])
+  pred_data = bdt.predict(d_data, output_margin=config["multiclassifier"])
+
+  # estimate z_a w/at least 4 signal events
+  n_quantiles = 20
+  signal_mva_scores = {"bdt_score" : ks_test.logical_vector(pred_test, y_test, 1)}
+  bkg_mva_scores = {"bdt_score" : ks_test.logical_vector(pred_test, y_test, 0)}
+  data_mva_scores = {"bdt_score" : pred_data}
+
+  signal_mass = ks_test.logical_vector(mass_validation, y_test, 1)
+  bkg_mass = ks_test.logical_vector(mass_validation, y_test, 0)
+
+  signal_weights = ks_test.logical_vector(weights_validation, y_test, 1)
+  bkg_weights = ks_test.logical_vector(weights_validation, y_test, 0)
+
+  optimization_vars = config["optimization_vars"].split(",") if config["optimization_vars"] else []
+  for var in optimization_vars:
+    signal_mva_scores[var] = ks_test.logical_vector(utils.load_array(f, var + '_validation'), y_test, 1)
+    bkg_mva_scores[var]    = ks_test.logical_vector(utils.load_array(f, var + '_validation'), y_test, 0)
+    data_mva_scores[var]   = utils.load_array(f, var + '_data')
+
+  signal_events = { "mass" : signal_mass, "weights" : signal_weights, "mva_score" : signal_mva_scores }
+  bkg_events = { "mass" : bkg_mass, "weights" : bkg_weights, "mva_score" : bkg_mva_scores }
+  data_events = { "mass" : mass_data, "weights" : weights_data, "mva_score" : data_mva_scores }
+
+  za, za_unc, s, b, sigma_eff = significance_utils.za_scores(n_quantiles, signal_events, bkg_events, False)
+  za_data, za_unc_data, s_data, b_data, sigma_eff_data = significance_utils.za_scores(n_quantiles, signal_events, data_events, True)
+  za = numpy.asarray(za)
+  za_data = numpy.asarray(za_data)
+
+  max_za_mc = numpy.max(za[numpy.where(numpy.asarray(s) >= 4.)])
+  max_za_data = numpy.max(za_data[numpy.where(numpy.asarray(s_data) >= 4.)])
+
+  max_za_mc, max_za_mc_idx = utils.find_nearest(za, max_za_mc)
+  max_za_data, max_za_data_idx = utils.find_nearest(za_data, max_za_data)
+
+  max_za_mc_unc   = za_unc[max_za_mc_idx]
+  max_za_data_unc = za_unc_data[max_za_data_idx]
+
+  print "Max Z_A on MC:   %.4f +/- %.4f" % (max_za_mc,   max_za_mc_unc)
+  print "Max Z_A on data: %.4f +/- %.4f" % (max_za_data, max_za_data_unc)
+
+  return max_za_mc, max_za_mc_unc, max_za_data, max_za_data_unc
