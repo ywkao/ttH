18e882f5a8be05ac620267eb3864b58d0518b75c
Samuel May <sammay@ucla.edu>
2019-06-07 11:07:09 -0700

    Forgot to add these scripts

---
+++ MVAs/optimize_dnn.py
@@ -0,0 +1,93 @@
+import os, sys
+import json
+
+from bayes_opt import BayesianOptimization
+
+sys.path.append("../Loopers")
+import parallel_utils
+import train_dnn_core
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help = "input hdf5 file", type=str)
+parser.add_argument("--tag", help = "save name for weights file", type=str)
+parser.add_argument("--channel", help = "Hadronic or Leptonic", type=str, default="Hadronic")
+parser.add_argument("--no_global", help = "don't use global features", action="store_true")
+parser.add_argument("--no_lstm", help = "don't use object features (lstm)", action="store_true")
+parser.add_argument("--load", help = "give weights file to use as starting point", type=str)
+args = parser.parse_args()
+
+
+def auc(n_nodes_dense_1, n_nodes_dense_2, n_dense_1, n_dense_2, n_nodes_lstm, n_lstm, maxnorm, dropout_rate, learning_rate, start_batch):
+    global idx, log, full_results
+
+    config = {}
+    config["n_nodes_dense_1"] = int(n_nodes_dense_1)
+    config["n_nodes_dense_2"] = int(n_nodes_dense_2)
+    config["n_dense_1"] = int(n_dense_1)
+    config["n_dense_2"] = int(n_dense_2)
+    config["n_nodes_lstm"] = int(n_nodes_lstm)
+    config["n_lstm"] = int(n_lstm)
+    config["maxnorm"] = maxnorm
+    config["dropout_rate"] = dropout_rate
+    config["learning_rate"] = learning_rate
+    config["start_batch"] = int(start_batch)
+
+    trained_dnn = train_dnn_core.train(args, config)
+
+    full_results[idx] = {"config" : config, "results" : {"auc_train" : trained_dnn.auc["train"], "auc_train_unc" : trained_dnn.auc_unc["train"], "auc_test" : trained_dnn.auc["validation"], "auc_test_unc" : trained_dnn.auc_unc["validation"]}}
+
+    with open(log, "w") as f_out:
+        json.dump(full_results, f_out, indent=4, sort_keys=True)
+
+    idx += 1
+    return trained_dnn.auc["validation"][-1]
+
+idx = 0
+log = "bayes_dnn_hyperparam_scan_%s_%s.json" % (args.channel, args.tag)
+full_results = {}
+
+pbounds = {
+    "n_nodes_dense_1" : (300, 300), 
+    "n_nodes_dense_2" : (200, 200), 
+    "n_dense_1" : (1,1), 
+    "n_dense_2" : (4,4), 
+    "n_nodes_lstm" : (25, 150), 
+    "n_lstm" : (1,5), 
+    "maxnorm" : (0.1, 100), 
+    "dropout_rate" : (0.0, 0.5), 
+    "learning_rate" : (0.000001, 1.0),
+    "start_batch" : (256, 2048)
+}
+
+starting_point = {
+    "n_nodes_dense_1" : 300, 
+    "n_nodes_dense_2" : 200, 
+    "n_dense_1" : 1, 
+    "n_dense_2" : 4, 
+    "n_nodes_lstm" : 100, 
+    "n_lstm" : 3, 
+    "maxnorm" : 3, 
+    "dropout_rate" : 0.25, 
+    "learning_rate" : 0.001,
+    "start_batch" : 512
+}
+
+optimizer = BayesianOptimization(
+    f=auc,
+    pbounds=pbounds,
+    verbose=2, # verbose = 1 prints only when a maximum is observed, verbose = 0 is silent
+    random_state=1,
+)
+
+optimizer.probe(params = starting_point, lazy = True)
+
+optimizer.maximize(
+        init_points = 1,
+        n_iter = 1000,
+        acq = "ei",
+        xi = 0.001,
+)
+
+for i, res in enumerate(optimizer.res):
+    print("Iteration {}: \n\t{}".format(i, res))
---
+++ MVAs/train_dnn_core.py
@@ -0,0 +1,77 @@
+import os
+import tensorflow as tf
+print(tf.__version__)
+config = tf.ConfigProto(log_device_placement=True)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+print(session)
+
+import keras
+import numpy
+import sys
+from sklearn import metrics
+import h5py
+import glob
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import dnn_helper
+import significance_utils
+import utils
+import ks_test
+
+def train(args, config):
+  f = h5py.File(args.input, "r")
+  if len(config.keys()) == 0:
+      config = {"n_nodes_dense_1" : 300, "n_nodes_dense_2" : 200, "n_dense_1" : 1, "n_dense_2" : 4, "n_nodes_lstm" : 100, "n_lstm" : 3, "maxnorm" : 3, "dropout_rate" : 0.25, "learning_rate" : 0.001, "start_batch" : 512} 
+
+
+  object_features, object_features_validation, object_features_data = f['object'], f['object_validation'], f['object_data']
+  if args.channel == "Leptonic":
+    jet_features, jet_features_validation, jet_features_data = f['jet'], f['jet_validation'], f['jet_data']
+    lepton_features, lepton_features_validation, lepton_features_data = f['lepton'], f['lepton_validation'], f['lepton_data']
+
+  global_features, global_features_validation, global_features_data = f['global'], f['global_validation'], f['global_data']
+  label, label_validation, label_data = f['label'], f['label_validation'], f['label_data']
+  weights, weights_validation, weights_data = f['weights'], f['weights_validation'], f['weights_data']
+  mass, mass_validation, mass_data = f['mass'], f['mass_validation'], f['mass_data']
+  top_tag_score, top_tag_score_validation, top_tag_score_data = f['top_tag_score'], f['top_tag_score_validation'], f['top_tag_score_data']
+  tth_ttPP_mva, tth_ttPP_mva_validation, tth_ttPP_mva_data = f['tth_ttPP_mva'], f['tth_ttPP_mva_validation'], f['tth_ttPP_mva_data']
+  tth_dipho_mva, tth_dipho_mva_validation, tth_dipho_mva_data = f['tth_dipho_mva'], f['tth_dipho_mva_validation'], f['tth_dipho_mva_data']
+  tth_std_mva, tth_std_mva_validation, tth_std_mva_data = f['tth_std_mva'], f['tth_std_mva_validation'], f['tth_std_mva_data']
+  evt_data = f['evt_data']
+  run_data = f['run_data']
+  lumi_data = f['lumi_data']
+
+
+  # Features
+  leptonic_single_lstm = True
+
+
+  if args.channel == "Hadronic" or leptonic_single_lstm:
+    dnn_features_train = dnn_helper.DNN_Features(name = 'train', global_features = global_features, objects = object_features, label = label, weights = weights, references = { "Top Tag Score" : top_tag_score, "ttPP BDT" : tth_ttPP_mva, "Dipho BDT" : tth_dipho_mva, "Baseline BDT" : tth_std_mva}, no_prep = True)
+    dnn_features_validation = dnn_helper.DNN_Features(name = 'validation', global_features = global_features_validation, objects = object_features_validation, label = label_validation, weights = weights_validation, references = { "Top Tag Score" : top_tag_score_validation, "ttPP BDT" : tth_ttPP_mva_validation, "Dipho BDT" : tth_dipho_mva_validation, "Baseline BDT" : tth_std_mva_validation}, no_prep = True)
+    dnn_features_data = dnn_helper.DNN_Features(name = 'data', global_features = global_features_data, objects = object_features_data, label = label_data, weights = weights_data, references = { "Top Tag Score" : top_tag_score_data, "ttPP BDT" : tth_ttPP_mva_data, "Dipho BDT" : tth_dipho_mva_data, "Baseline BDT" : tth_std_mva_data}, no_prep = True)
+
+  else:
+    dnn_features_train = dnn_helper.DNN_Features(name = 'train', global_features = global_features, objects = jet_features, leptons = lepton_features, label = label, weights = weights, references = { "Top Tag Score" : top_tag_score, "ttPP BDT" : tth_ttPP_mva, "Dipho BDT" : tth_dipho_mva, "Baseline BDT" : tth_std_mva}, no_prep = True)
+    dnn_features_validation = dnn_helper.DNN_Features(name = 'validation', global_features = global_features_validation, objects = jet_features_validation, leptons = lepton_features_validation, label = label_validation, weights = weights_validation, references = { "Top Tag Score" : top_tag_score_validation, "ttPP BDT" : tth_ttPP_mva_validation, "Dipho BDT" : tth_dipho_mva_validation, "Baseline BDT" : tth_std_mva_validation}, no_prep = True)
+    dnn_features_data = dnn_helper.DNN_Features(name = 'data', global_features = global_features_data, objects = jet_features_data, leptons = lepton_features_data, label = label_data, weights = weights_data, references = { "Top Tag Score" : top_tag_score_data, "ttPP BDT" : tth_ttPP_mva_data, "Dipho BDT" : tth_dipho_mva_data, "Baseline BDT" : tth_std_mva_data}, no_prep = True)
+
+  # DNN Helper
+  dnn = dnn_helper.DNN_Helper(features_train = dnn_features_train, features_validation = dnn_features_validation, features_data = dnn_features_data, tag = args.tag, evt_data = evt_data, run_data = run_data, lumi_data = lumi_data, mass_data = mass_data, config = config)
+
+  if args.load:
+    dnn.weights_file = args.load
+    dnn.debug()
+
+  else:
+    # Train
+    dnn.train_with_early_stopping()
+
+    # Diagnostics
+    dnn.do_diagnostics()
+  return dnn
