80fca796e6f9441078f5ddb4c37b5a0cac2a5de9
Samuel May <sammay@ucla.edu>
2019-06-10 16:09:53 -0700

    Pull git repo manually in nautilus jobs

--- MVAs/nautilus/nautilus_exe.sh
+++ MVAs/nautilus/nautilus_exe.sh
@@ -2,7 +2,6 @@
 # 1. Make pod: kubectl create -f optimize_dnn_hyperparams.yaml 
 # 2. Connect to pod: kubectl exec -it mytensorflow-sam  -n cms -- /bin/bash
 cd ../
-apt-get install wget
 apt-get install openssh-client
 apt-get install vim
 apt-get install git
